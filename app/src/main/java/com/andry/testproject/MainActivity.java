2c0ef19d44c053e2d835e04b959fb6a5eeec0399
Andry <andry.jose.v@gmail.com>
2018-10-16 20:03:51 -0400

    Initial commit

---
+++ app/src/main/java/com/andry/testproject/MainActivity.java
@@ -0,0 +1,42 @@
+package com.andry.testproject;
+
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText textr1,textr2;
+    Button buttonUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonUp = findViewById(R.id.upload_data);
+
+        buttonUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textr1 = findViewById(R.id.dato1);
+                textr2 = findViewById(R.id.dato2);
+
+                if (textr1.getText().toString().isEmpty()&&textr2.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Todo vacio", Toast.LENGTH_SHORT).show();
+                } else if (textr1.getText().toString().isEmpty()){
+                    textr1.setError("Campo Oblidado");
+                } else if (textr2.getText().toString().isEmpty()){
+                    textr2.setError("obligado");
+                }else {
+                    Toast.makeText(MainActivity.this, "Todo bn parce", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+}
