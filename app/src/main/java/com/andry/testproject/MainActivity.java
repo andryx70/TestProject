0a00d9143f940de120fd83fa93e2a2dcc695dd8f
Andry <andry.jose.v@gmail.com>
2019-10-08 20:45:58 -0400

    prueba terminadas commit para no extraviar el codigo

--- app/src/main/java/com/andry/testproject/MainActivity.java
+++ app/src/main/java/com/andry/testproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.andry.testproject;
 
+import android.content.Intent;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,7 +11,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     TextInputEditText textr1,textr2;
-    Button buttonUp;
+    Button buttonUp, otherActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         buttonUp = findViewById(R.id.upload_data);
+        otherActivity = findViewById(R.id.next_activity);
 
         buttonUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +36,15 @@ public class MainActivity extends AppCompatActivity {
                 }else {
                     Toast.makeText(MainActivity.this, "Todo bn parce", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        otherActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Upload_Data_Activity.class);
+                startActivity(intent);
             }
         });
 
